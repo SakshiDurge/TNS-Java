4f5a30289af85d934ba959cbb76af6d909ac8aed
SakshiDurge <sakshidurge0208@gmail.com>
2025-01-30 23:08:43 +0530

    Data types added

---
+++ TNS/src/DayOne/PrimitiveTypes.java
@@ -0,0 +1,13 @@
+package DayOne;
+
+public class PrimitiveTypes {
+
+	public static void main(String[] args) {
+		int a=13;
+		System.out.println(a);
+
+		float b=9/2f;
+		System.out.println(b);
+	}
+
+}
