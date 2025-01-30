0f03bb349632eaff04693f60e63df11dbae5306f
SakshiDurge <sakshidurge0208@gmail.com>
2025-01-30 23:18:05 +0530

    added code

---
+++ TNS/src/DayOne/data.java
@@ -0,0 +1,18 @@
+package DayOne;
+
+public class data {
+	public static void main(String[]args) {
+		
+		int $num1=13;
+		long l=12658733;
+		int num=24;
+		int FOR=23;
+		String nameString="sakshi";
+		
+		System.out.println($num1);
+	    System.out.println(l);
+		System.out.println(num);
+		System.out.println(nameString);
+		System.out.println(FOR);
+	}
+}
