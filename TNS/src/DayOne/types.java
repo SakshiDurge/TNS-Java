fc7f0b63b5b3ac05f1b88e08d9ba65f6f5ed4fac
SakshiDurge <sakshidurge0208@gmail.com>
2025-01-30 23:23:09 +0530

    added

---
+++ TNS/src/DayOne/types.java
@@ -0,0 +1,31 @@
+package DayOne;
+
+public class types {
+	public static void main(String[]args) {
+		// byte
+		byte byteMax=127;
+		byte byteMin=-128;
+		System.out.println(byteMax);
+		System.out.println(byteMin);
+		
+		//2 byte
+		short shortMax=32767;
+		short shortMin=-23767;
+		System.out.println(shortMax);
+		System.out.println(shortMin);
+		
+		//4 byte
+		int intMax=2147483647;
+		int intMin=-2147483648;
+		System.out.println(intMax);
+		System.out.println(intMin);
+		
+		//8 byte
+		long longMax=9223372036854775807L;
+		long longMin=-9223372036854775808L;
+		System.out.println(longMax);
+		System.out.println(longMin);
+		
+	}
+
+}
