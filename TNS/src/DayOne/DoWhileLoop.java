7c4ccbbf12303ef197300a37e0ba58bdc1461955
SakshiDurge <sakshidurge0208@gmail.com>
2025-02-09 16:12:58 +0530

    do while loop

--- TNS/src/DayOne/DoWhileLoop.java
+++ TNS/src/DayOne/DoWhileLoop.java
@@ -3,7 +3,14 @@ package DayOne;
 public class DoWhileLoop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int i=-11;
+		do {
+			System.out.println(i);
+			i++;
+			
+		}
+		while(i<=1);
 
 	}
 
