1207bc9d10de455f93f7ac7759b0a709bcf0cad5
SakshiDurge <sakshidurge0208@gmail.com>
2025-02-09 16:35:30 +0530

    While Loop

---
+++ TNS/src/DayOne/WhileLoopDemo.java
@@ -0,0 +1,18 @@
+package DayOne;
+
+public class WhileLoopDemo {
+
+	public static void main(String[] args) {
+		
+		
+		int i=101;
+		
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
--- TNS/src/DayOne/operator.java
+++
@@ -1,5 +0,0 @@
-package DayOne;
-
-public class operator {
-
-}
