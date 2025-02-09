3844ed84e504b352b303441d0aeda6d0ae00495c
SakshiDurge <sakshidurge0208@gmail.com>
2025-02-09 16:24:53 +0530

    nested loop

---
+++ TNS/src/DayOne/NestedForLoop.java
@@ -0,0 +1,20 @@
+package DayOne;
+
+public class NestedForLoop {
+
+	public static void main(String[] args) {
+		
+		int age=20;
+		
+		if(age> 18)
+		{
+			System.out.println("You are eligible for voting");
+		}
+		else 
+		{
+			System.out.println("Sorry! You need to wait more ");
+		}
+
+	}
+
+}
